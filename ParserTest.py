48445864149b0c5ea97aaf6b78b4e85b5c2655cc
David Müller <s.david.mueller@gmx.de>
2019-11-12 14:24:01 +0100

    Updated ResponseParser and added ParserTest

---
+++ ParserTest.py
@@ -0,0 +1,25 @@
+#Tests for ResponseParser class
+#
+
+from HttpClient import HttpClient 
+from ResponseParser import ResponseParser
+
+
+testUrl = "https://10minutemail.net/"
+
+testClient = HttpClient()
+testParser = ResponseParser()
+
+testResponse = testClient.initialRequest(testUrl)
+
+#Test getFakeAccData method
+print( testParser.getFakeAccData(testResponse))
+
+#Test getInboxLinks method
+print( testParser.getInboxLinks(testResponse))
+
+#Test ... method
+# print()
+
+
+
---
+++ ResponseParser.py
@@ -0,0 +1,49 @@
+# Handles http responses and parses for 
+# requested data.
+# This parser is currently only able to handle responses 
+# from "https://10minutemail.net/".
+# 
+import re
+from bs4 import BeautifulSoup as bso
+
+
+class ResponseParser():
+
+    # Returns the data needed by a FakeMailAccount Object
+    def getFakeAccData(self, response):               
+            beautifulContent = bso(response.content, "html.parser")
+            
+            mailAdress = self.getMailAdress(beautifulContent)
+            cookies = self.getCookies(response)
+
+            return mailAdress, cookies
+        
+
+    # Get the e-mail adress of the fake account
+    def getMailAdress(self, content):
+            inputElement = content.find("input")
+            mailAdress   = inputElement.get("value")
+
+            return mailAdress
+
+
+    # Return all cookies as a dictionary: ["key":"value",...]
+    def getCookies(self, response):
+            sessionCookies = response.cookies.get_dict()
+    
+            return sessionCookies
+
+
+    # Return a list of the link endings for the mails 
+    def getInboxLinks(self, response):
+            beautifulContent = bso(response.content, "html.parser")
+            inboxElements = beautifulContent.find("table")
+            trimmedLinks = []
+            
+            for mails in inboxElements.contents:
+                link = mails.attrs.get('onclick')
+                if (link):
+                    extractedLink = re.findall("readmail.html.*'", link)
+                    trimmedLinks.append(extractedLink[0].strip("'"))
+            return trimmedLinks
+
